b525c0c565333572c7b2b67a7cad824d44e35ef6
Matthias Noback <matthiasnoback@gmail.com>
2017-05-03 12:16:04 +0200

    Fix a small mistake

--- src/MeetupOrganizing/Domain/Model/Meetup/OrganizerId.php
+++ src/MeetupOrganizing/Domain/Model/Meetup/OrganizerId.php
@@ -4,16 +4,8 @@ declare(strict_types=1);
 namespace MeetupOrganizing\Domain\Model\Meetup;
 
 use Common\DomainModel\AggregateId;
-use Membership\Domain\Model\User\UserId;
 
 final class OrganizerId
 {
     use AggregateId;
-
-    public function asUserId(): UserId
-    {
-        return UserId::fromString(
-            $this->__toString()
-        );
-    }
 }
